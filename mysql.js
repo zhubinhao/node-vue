9e07ddd8b2a2c9b18ab047e2cdacb7af11ccd1b5
zbh <zhubinhaohll@163.com>
2018-10-26 17:25:06 +0800

    失误啊

--- mysql.js
+++ mysql.js
@@ -1,8 +1,5 @@
 const mysql = require('mysql');
-const HOST = '45.32.100.27';
-const USER = 'root';
-const PASS = '091794';
-const DATABASE = 'text';
+
 const db = mysql.createPool({
     host: HOST,
     user: USER,
